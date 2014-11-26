468fdb9beccfdad070f44a368759579c5ef79916
Jamis Johnson <jamismanwaring@gmail.com>
2014-11-25 20:33:51 -0500

    adds background-image

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require('express')
 var app = express();
 
+// app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('B o_0 bie trap! ╭∩╮ʕ•ᴥ•ʔ╭∩╮')
-})
+var appPath = __dirname + '/client';
+
+app.get('/', function(req, res) { 
+  res.render(appPath + '/index')
+});
+app.use('/img', express.static(appPath + '/img/'));
+app.use('/css', express.static(appPath + '/css/'));
+app.use('/js',  express.static(appPath + '/js/'));
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
